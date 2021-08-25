36fc0a7658b77c5a14e10561fd081fe876506027
Reveta <gfdsre1999@gmail.com>
2021-08-25 16:52:33 +0300

    Init

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Drawing;
+using OpenCvSharp;
+
+namespace HelloWorld {
+	static class Program 
+	{
+		static void Main() 
+		{
+			var path = @"C:\Users\gfdsr\OneDrive\prFiles\job\Freelance\OpenCVOrigin\cSharp\HelloWorld\HelloWorld\media\project\proj_2\Chip_001.jpg";
+			using var src = new Mat(path, ImreadModes.Grayscale);
+			using var dst = new Mat();
+        
+			Cv2.Canny(src, dst, 50, 200);
+			using (new Window("src image", src)) 
+			using (new Window("dst image", dst)) 
+			{
+				Cv2.WaitKey();
+			}
+		}
+	}
+}
