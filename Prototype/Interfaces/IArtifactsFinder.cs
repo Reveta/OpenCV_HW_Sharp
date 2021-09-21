588dc29573fe9f7fca13295468ffc79e4161dcc3
Reveta <gfdsre1999@gmail.com>
2021-09-21 18:09:07 +0300

    Config Correction

--- Prototype/Interfaces/IArtifactsFinder.cs
+++
@@ -1,9 +0,0 @@
-﻿using OpenCvSharp;
-
-namespace Prototype.Interfaces {
-	public interface IArtifactsFinder {
-		(Mat originalBlobs, Mat maskBlobs) Analise(Mat originalPhoto);
-
-		// bool IsBlobs(Mat ana);
-	}
-}
